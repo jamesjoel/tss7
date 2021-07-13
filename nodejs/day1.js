b6bae5fb4a039323853c5c2f3ccc1f2c52854c2a
jamesjoel <james.joelj@gmail.com>
2021-07-13 20:09:44 +0530

    node day 1

---
+++ nodejs/day1.js
@@ -0,0 +1,29 @@
+// ----- single line commant
+
+/*
+JavaScript is a case-sencentive lag.
+
+-------- Variables in Node (JavaScript)
+
+the variables are used for storing or holding information/data.
+
+
+Roules for Nameing a Variables
+
+1. The var name can not strat with number.
+(7a, 10b, 5c --- not used     but a7, b10, c5 allowed)
+
+2. Tha var name can not used any spacial char.
+(#, @, $, %, ^, &, *, space not used but _ accapted)
+
+3. var name also case-sencative
+( var a,    var A )
+
+
+*/
+
+var a = 100;
+var b = 200;
+
+var c = a+b;  
+console.log(c);
---
+++ nodejs/day2.js
@@ -0,0 +1,61 @@
+/*
+------- Data-Types of Variales --------
+
+1. intiger (number)
+
+2. float (decimal number)
+
+3. boolean (true/false)
+
+4. string (char, coll of any char)
+
+5. array
+
+6. object
+
+7. JSON
+
+8. null
+
+9. empty
+
+10. undefined
+
+*/
+
+
+// var a = 150;
+// this var data-tyep is intiger
+
+// var a = 150.23;
+// this var data-tyep is float
+
+// var a = 23;
+// var b = 2.5;
+
+// var c = a/b;
+
+// console.log(c);
+
+// var a = true;
+// var b = false;
+// this var is boolean
+
+
+var a = "abc";
+var name = "amar joshi";
+var city = 'indore';
+// this var is string data-type
+// console.log(city);
+// console.log("india");
+
+
+
+
+var abc = "rohit";
+// console.log(abc);
+console.log("abc");
+
+
+
+
