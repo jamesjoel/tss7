5da42bd2490ffd2612da3e432c3229e23d384977
jamesjoel <james.joelj@gmail.com>
2021-07-15 14:25:26 +0530

    array

---
+++ nodejs/array.js
@@ -0,0 +1,19 @@
+/*
+    when we want multipal values/data store in a single var, then
+    we can use array data-type.
+
+    An array can store any data-types of values.
+*/
+
+//var a = []; // create a empty array
+
+var a = [10, 30, 50, 80, 100, 55.36, true, "rohit"];
+
+// every values/data in array have a unique address
+// any we can call this unique address --- Index Number
+
+// console.log(a[7]);
+console.log(a[2]+a[3]); // 130  
+
+
+
---
+++ nodejs/string.js
@@ -0,0 +1,29 @@
+var a = "245";
+console.log(a);
+/*
+    Data-Types
+
+1. intiger (number)
+
+2. float (decimal number)
+
+3. boolean (true/false)
+
+4. string (char, coll of any char)
+
+
+5. array
+
+6. object
+
+7. JSON
+
+
+
+8. null
+
+9. empty
+
+10. undefined
+
+*/
