69c31e40dc98c938e1d681fcc447b0533b47038f
jamesjoel <james.joelj@gmail.com>
2021-07-15 20:10:36 +0530

    object

---
+++ nodejs/object.js
@@ -0,0 +1,28 @@
+// an object can also store mult values
+
+var x = ["asad", "moha", "priyanka"];
+// x[2]
+
+var a = { a : "asad", b : "moha", c : "priyanka", d : 100, e: true  }; // empty object
+
+
+var obj = { name : "rohit", age : 25, city : "indore" };
+
+console.log(obj);
+obj.gender = "male";
+console.log(obj);
+
+
+/*
+    variables
+
+    data-type
+
+    value
+
+
+    Cup(vairalbes)
+
+    Tea , Coffee (value)
+
+*/
---
+++ nodejs/operator.js
@@ -0,0 +1,87 @@
+/*
+-------------- Operators in JS -----------------
+
+    Arithemetic Ope
+
+        +       sum
+
+        -       subs
+
+        /       div
+
+        *       mutiply
+
+        %       module
+
+    
+    Assinment Ope
+
+        =
+
+        +=
+
+        -=
+
+        *=
+
+        /=
+
+        %=
+
+    
+    Inc/Dec ope
+
+        ++
+
+        --
+
+    
+    compairession ope
+
+        ==          is equal
+
+        !=          is not equal
+
+        <           less then
+
+        >           greater then
+
+        <=          less then and equal
+
+        >=          greater then and equal
+
+    
+    Logical Ope
+
+        &&      AND
+
+        ||      OR
+
+        !       NOT
+
+*/
+/*
+var a = 10;
+var b = 5;
+// a = a * b;
+a *= b;
+*/
+/*
+var a = 26;
+var b = 3;
+a %= b;
+*/
+
+var a = 5;
+a++;
+a++;
+a++;
+a--;
+a--;
+a++;
+console.log(a);
+
+
+
+
+
