2582553f7c91842f3f8cc5e048aec1d8c82c9304
jamesjoel <james.joelj@gmail.com>
2021-07-17 19:51:59 +0530

    switch

---
+++ nodejs/switch.js
@@ -0,0 +1,36 @@
+/*
+    switch(a)
+    {
+        case condi : task;
+
+        case condi  : task;
+
+        
+
+    }
+
+*/
+var a = 3;
+
+
+
+
+switch(a)
+{
+    case 1 : console.log("Indore");
+            break;
+            
+    case 2 : console.log("Mumbai");
+            break;
+            
+    case 3 : console.log("Ujjain");
+            break;
+            
+    case 4 : console.log("Delhi");
+            break;
+            
+    case 5 : console.log("Pune");
+            break;
+
+    default : console.log("No City");
+}
---
+++ nodejs/switch2.js
@@ -0,0 +1,25 @@
+var a = "x";
+
+
+switch(a) // a e i o u
+{
+    case "a" : console.log("A is vowel");
+            break;
+    
+    case "e" : console.log("E is vowel");
+            break;
+    
+    case "i" : console.log("I is vowel");
+            break;
+    
+    case "o" : console.log("O is vowel");
+            break;
+    
+    case "u" : console.log("U is vowel");
+            break;
+
+    default : console.log("the value is consonant");
+}
+/*
+    var a = 4;
+*/
