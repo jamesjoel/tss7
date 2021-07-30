37b6cd305ab1137c143676182535b0cfcd4441e0
jamesjoel <james.joelj@gmail.com>
2021-07-30 20:06:54 +0530

    express.static()

---
+++ nodejs/pro8/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+
+app.use(express.static(__dirname+"/assets"));
+// the app.use()function call auto, before any other 'routes' call
+
+// the express.static()function call all static files for HTML page.
+
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname+"/home.html");
+})
+app.get("/about", (req, res)=>{
+    res.sendFile(__dirname+"/about.html");
+})
+
+app.listen(3000, ()=>{
+    console.log("server running");
+})
+
+//EJS
---
+++ nodejs/pro8/demo.js
@@ -0,0 +1,7 @@
+function demo(a, b)
+{
+    var c = a+b;
+    return c;
+}
+
+var x = demo(10, 20);
