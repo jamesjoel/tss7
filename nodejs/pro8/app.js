a3ace625472d32905b3c275547ebf8939295e271
jamesjoel <james.joelj@gmail.com>
2021-07-31 20:25:17 +0530

    ejs

--- nodejs/pro8/app.js
+++ nodejs/pro8/app.js
@@ -5,6 +5,8 @@ var app = express();
 app.use(express.static(__dirname+"/assets"));
 // the app.use()function call auto, before any other 'routes' call
 
+// localhost:3000/about
+
 // the express.static()function call all static files for HTML page.
 
 
---
+++ nodejs/pro9/app.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/assets"));
+
+app.get("/", (req, res)=>{
+    var a = "indore";
+    
+    var obj = { x : a, y : "rohit" };
+
+    res.render("home", obj);
+    // res.sendFile(__dirname+"/home.html")
+})
+
+
+app.get("/about", (req, res)=>{
+    var name = "nidhi";
+    var age = 25;
+    var gender = "female";
+    var obj = { name : name, age : age, gender : gender };
+    res.render("about", obj);
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log("server running");
+})
+/*
+    html  ----- ejs (view engine)  ---- show the varialbes
+
+    npm init
+    npm i express
+    npm i ejs
+
+    1. npm i ejs
+
+    2. app.set("view engine", "ejs")
+
+    3. create a folder "views"
+
+    4. all .html file convert to .ejs file, and save it to "views" folder
+
+    5. all values you want to print in ".ejs" file, so you have to crate
+    a object with those values.
+*/
