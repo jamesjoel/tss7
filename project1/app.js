3fc11843c1a8f317fbef8415f276fa85b9f0c216
jamesjoel <james.joelj@gmail.com>
2021-08-10 20:08:34 +0530

    insert in mongodb

--- project1/app.js
+++ project1/app.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var app = express();
 var routes = require("./config/routes");
+var bodyParser = require("body-parser");
+
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/assets"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(routes);
 
--- project1/controllers/signup.js
+++ project1/controllers/signup.js
@@ -1,8 +1,27 @@
 var express = require("express");
 var routes = express.Router();
+// npm i mongodb 
+var MongoClient = require("mongodb").MongoClient;
 
 routes.get("/", (req, res)=>{
     res.render("signup");
+});
+
+routes.post("/save", (req, res)=>{
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss7");
+        db.collection("user").insertOne(req.body, (err, result)=>{
+            res.redirect("/");
+            // INSERT INTO
+        })
+    })
 })
 
 module.exports = routes;
+
+/*
+    Node ----------------------> DataBase Connection(MongoDB)
+
+    Node ----------------------> DataBase Connection(MySQL)
+
+*/
