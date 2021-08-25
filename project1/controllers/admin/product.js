f2c11fb6e5585e21ae890f6afefd9e1cda667eb4
jamesjoel <james.joelj@gmail.com>
2021-08-25 20:09:49 +0530

    delete product

--- project1/controllers/admin/product.js
+++ project1/controllers/admin/product.js
@@ -3,6 +3,7 @@ const { route } = require("./login");
 var routes = express.Router();
 var MongoClient = require("mongodb").MongoClient;
 var mongoUrl = "mongodb://localhost:27017";
+var mongodb = require("mongodb");
 
 // location:3000/admin/product/add
 routes.get("/add", (req, res)=>{
@@ -46,5 +47,22 @@ routes.post("/add", (req, res)=>{
     })
 })
 
+
+routes.get("/delete/:x", (req, res)=>{
+    //console.log(req.params);
+
+    var _id = mongodb.ObjectId(req.params.x);
+
+    // console.log("delete product --------------");
+    MongoClient.connect(mongoUrl, (err, con)=>{
+        var db = con.db("tss7");
+        db.collection("product").remove({_id : _id}, (err, result)=>{
+            if(err){
+                console.log(err);
+            }
+            res.redirect("/admin/product/list");
+        })
+    })
+})
 
 module.exports = routes;
